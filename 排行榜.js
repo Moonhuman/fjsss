be98fce3368c6b1d6e93c7e63a6d0cb895cea92e
Moonhuman <55131813+Moonhuman@users.noreply.github.com>
2019-10-30 20:21:02 +0800

    福建十三水

---
+++ 排行榜.js
@@ -0,0 +1,32 @@
+$(function(){
+    var data=new Array();
+    $.ajax({
+            url: "http://api.revth.com/rank/rank.json",
+            // beforeSend: function(xhr) {
+            //     xhr.setRequestHeader("X-Auth-Token",token);
+            // },
+    
+            type: "GET",
+            success: function (result) {
+                data=result;
+                console.log(data);
+                renderPage()
+            },
+            error: function (res) {
+                alert("查看排行失败");
+            }
+        });
+    function renderPage() {
+		var i=1;
+        data.forEach((item) => {
+            $("#rank-table").append(
+            "<tr align='center'>" +
+				"<td>" + String(i) + "</td> " + 
+                "<td>" + item["player_id"] + "</td> " + 
+                "<td>" + item["name"] + "</td> " +
+                "<td>" + item["score"] + "</td> " +
+            "</tr>")
+			i++;
+        })
+    } 
+})
